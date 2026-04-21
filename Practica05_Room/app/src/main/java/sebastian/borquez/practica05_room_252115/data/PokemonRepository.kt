9462131a576fb9d9d5de6c64372deefac1659086
SEHUZ <sonic15622@gmail.com>
2026-04-21 06:35:20 -0700

    problemas 1
    borre todo

--- Practica05_Room/app/src/main/java/sebastian/borquez/practica05_room_252115/data/PokemonRepository.kt
+++
@@ -1,9 +0,0 @@
-package sebastian.borquez.practica05_room_252115.data
-
-class PokemonRepository(private val pokemonDao: PokemonDao) {
-    val allPokemons = pokemonDao.getAll()
-
-    suspend fun add(pokemon: PokemonEntity) {
-        pokemonDao.add(pokemon)
-    }
-}
